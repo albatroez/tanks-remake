aca166b99fab5871ab47713cc4b5c02c9dafeb78
Kkoonnrraadd <56445991+Kkoonnrraadd@users.noreply.github.com>
2020-05-31 14:02:23 +0200

    Destroy enemy with one issue and basic health script

--- Assets/Scripts/ShellShooter.cs
+++ Assets/Scripts/ShellShooter.cs
@@ -30,4 +30,12 @@ public class ShellShooter : MonoBehaviour
         
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
 }
