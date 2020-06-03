00d85b7afaa1273a7c7079b4c8cb00acd0630bd5
Kkoonnrraadd <56445991+Kkoonnrraadd@users.noreply.github.com>
2020-06-03 21:56:48 +0200

    UI

--- Assets/Scripts/BoomToPlayer.cs
+++ Assets/Scripts/BoomToPlayer.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoomBoom : MonoBehaviour
+public class BoomToPlayer : MonoBehaviour
 {
-    public int damage = 50;
+    public int damage = 10;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Player"))
         {
             //Destroy(other.gameObject);
             other.gameObject.SendMessage("OnDamage", damage);
--- Assets/Scripts/GenerateEnemy.cs
+++ Assets/Scripts/GenerateEnemy.cs
@@ -7,7 +7,7 @@ public class GenerateEnemy : MonoBehaviour
     // public GameObject theEnemy;
     public int xPos;
     public int zPos;
-    public int enemyCount = 10;
+    public int enemyCount = 0;
     private int currentNumberOfEnemies = 0;
     //private Rigidbody rigidbody;
 
@@ -30,7 +30,7 @@ public class GenerateEnemy : MonoBehaviour
             Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
             
             enemyCount += 1;
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(2f);
             
         }
     }
