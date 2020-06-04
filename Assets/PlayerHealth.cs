4ed84f54d4cfbbd6d3c4c50beb23ae087c2f247e
Kkoonnrraadd <56445991+Kkoonnrraadd@users.noreply.github.com>
2020-06-04 15:20:53 +0200

    update

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currrentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currrentHealth = maxHealth;
+        healthBar.SetMaxValue(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currrentHealth -= damage;
+        healthBar.SetHealth(currrentHealth);
+    }
+}
